86c35e0ae17a167a6c3ee5ad7a3382d3427a412f
Igor <igorsbueno010@gmail.com>
2024-08-23 21:19:01 -0300

    new

---
+++ Front-end/Aula22/js/app.js
@@ -0,0 +1,231 @@
+
+
+var prod2 =0.0
+var qtd2 =0.0
+
+var prod3 =0.0
+var qtd3 =0.0
+
+var prod4 =0.0
+var qtd4 =0.0
+
+var prod5 =0.0
+var qtd5 =0.0
+
+var prod6 =0.0
+var qtd6 =0.0
+
+var prod7 =0.0
+var qtd7 =0.0
+
+var prod8 =0.0
+var qtd8 =0.0
+
+var prod9 =0.0
+var qtd9 =0.0
+
+var prod10 =0.0
+var qtd10 =0.0
+
+var prod11 =0.0
+var qtd11 =0.0
+
+var prod12 =0.0
+var qtd12 =0.0
+
+var prod13 =0.0
+var qtd13 =0.0
+
+var prod14 =0.0
+var qtd14 =0.0
+
+var prod15 =0.0
+var qtd15 =0.0
+
+var prod16 =0.0
+var qtd16 =0.0
+
+var prod17 =0.0
+var qtd17 =0.0
+
+var prod18 =0.0
+var qtd18 =0.0
+
+var prod19 =0.0
+var qtd19 =0.0
+
+var prod20 =0.0
+var qtd20 =0.0
+
+var prod21 =0.0
+var qtd21 =0.0
+
+var prod22 =0.0
+var qtd22 =0.0
+
+var prod23 =0.0
+var qtd23 =0.0
+
+var prod24 =0.0
+var qtd24 =0.0
+
+var prod25 =0.0
+var qtd25 =0.0
+
+var prod26 =0.0
+var qtd26 =0.0
+
+var prod27 =0.0
+var qtd27 =0.0
+
+var total1 = 0.0;  
+var total2 = 0.0;  
+var total3 = 0.0;   
+var total4 = 0.0;   
+var total5 = 0.0;   
+var total6 = 0.0;  
+var total7 = 0.0;  
+var total8 = 0.0;  
+var total9 = 0.0;  
+var total10 = 0.0; 
+var total11 = 0.0; 
+var total12 = 0.0; 
+var total13 = 0.0; 
+var total14 = 0.0; 
+var total15 = 0.0; 
+var total16 = 0.0;  
+var total17 = 0.0;  
+var total18 = 0.0; 
+var total19 = 0.0; 
+var total20 = 0.0;  
+var total21 = 0.0; 
+var total22 = 0.0; 
+var total23 = 0.0; 
+var total24 = 0.0; 
+var total25 = 0.0;  
+var total26 = 0.0; 
+var total27 = 0.0;  
+
+
+
+
+function refrigeranteCalc(prod, qtd) {
+    var qtd1 = parseFloat(qtd)
+    var prod1 = parseFloat(prod) 
+    var total1 = qtd1 * prod1;
+    document.getElementById('subRefrigerante').value = total1
+}
+
+function cebolaCalc(prod, qtd){
+    prod = document.getElementsById('').value;
+    prod = parseFloat(prod1)
+    qtd = document.getElementsById('').value
+    total 
+}
+function ovosCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function amacianteCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function sabaoEmPoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function detergenteCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function shampooCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function saboneteCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function papelHigienicoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function cremeDentalCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function acucarCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function arrozCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function feijaoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function oleoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function macarraoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function cafeCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function filtroCafeCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function leiteCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function manteigaCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function margarinaCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function yogurteCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+// function biscoitoRecheadoCalc(prod, qtd)
+// prod = document.getElementsByName('').value;
+// qtd{} = document.getElementsByName('').value
+// total 
+function biscoitoSalgadoCalc(prod, qtd){
+    prod = document.getElementsByName('').value;
+    qtd = document.getElementsByName('').value
+    total 
+}
+function paoFormaCalc(prod, qtd){
+
+}
