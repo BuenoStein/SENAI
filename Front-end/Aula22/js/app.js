5305da315d8428b7e81c4dc8e1582a569ce5c1b4
Igor <igorsbueno010@gmail.com>
2024-08-28 14:28:51 -0300

    new

--- Front-end/Aula22/js/app.js
+++ Front-end/Aula22/js/app.js
@@ -1,231 +1,126 @@
 
-
-var prod2 =0.0
-var qtd2 =0.0
-
-var prod3 =0.0
-var qtd3 =0.0
-
-var prod4 =0.0
-var qtd4 =0.0
-
-var prod5 =0.0
-var qtd5 =0.0
-
-var prod6 =0.0
-var qtd6 =0.0
-
-var prod7 =0.0
-var qtd7 =0.0
-
-var prod8 =0.0
-var qtd8 =0.0
-
-var prod9 =0.0
-var qtd9 =0.0
-
-var prod10 =0.0
-var qtd10 =0.0
-
-var prod11 =0.0
-var qtd11 =0.0
-
-var prod12 =0.0
-var qtd12 =0.0
-
-var prod13 =0.0
-var qtd13 =0.0
-
-var prod14 =0.0
-var qtd14 =0.0
-
-var prod15 =0.0
-var qtd15 =0.0
-
-var prod16 =0.0
-var qtd16 =0.0
-
-var prod17 =0.0
-var qtd17 =0.0
-
-var prod18 =0.0
-var qtd18 =0.0
-
-var prod19 =0.0
-var qtd19 =0.0
-
-var prod20 =0.0
-var qtd20 =0.0
-
-var prod21 =0.0
-var qtd21 =0.0
-
-var prod22 =0.0
-var qtd22 =0.0
-
-var prod23 =0.0
-var qtd23 =0.0
-
-var prod24 =0.0
-var qtd24 =0.0
-
-var prod25 =0.0
-var qtd25 =0.0
-
-var prod26 =0.0
-var qtd26 =0.0
-
-var prod27 =0.0
-var qtd27 =0.0
-
-var total1 = 0.0;  
-var total2 = 0.0;  
-var total3 = 0.0;   
-var total4 = 0.0;   
-var total5 = 0.0;   
-var total6 = 0.0;  
-var total7 = 0.0;  
-var total8 = 0.0;  
-var total9 = 0.0;  
-var total10 = 0.0; 
-var total11 = 0.0; 
-var total12 = 0.0; 
-var total13 = 0.0; 
-var total14 = 0.0; 
-var total15 = 0.0; 
-var total16 = 0.0;  
-var total17 = 0.0;  
-var total18 = 0.0; 
-var total19 = 0.0; 
-var total20 = 0.0;  
-var total21 = 0.0; 
-var total22 = 0.0; 
-var total23 = 0.0; 
-var total24 = 0.0; 
-var total25 = 0.0;  
-var total26 = 0.0; 
-var total27 = 0.0;  
-
-
-
-
 function refrigeranteCalc(prod, qtd) {
-    var qtd1 = parseFloat(qtd)
-    var prod1 = parseFloat(prod) 
-    var total1 = qtd1 * prod1;
-    document.getElementById('subRefrigerante').value = total1
+    
+    var total1 = qtd * prod;
+    document.getElementById('subRefrigerante').value = total1.toFixed(2)
 }
 
 function cebolaCalc(prod, qtd){
-    prod = document.getElementsById('').value;
-    prod = parseFloat(prod1)
-    qtd = document.getElementsById('').value
-    total 
+
+    var total2 = qtd * prod;
+    document.getElementById('subCebola').value = total2.toFixed(2)
+
 }
+
 function ovosCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+    total3 = qtd * prod;
+    document.getElementById('subOvos').value = total3.toFixed(2)
 }
 function amacianteCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+    total4 = qtd * prod
+    document.getElementById('subAmaciante').value = total4.toFixed(2)
 }
-function sabaoEmPoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function sabaoEmPoCalc(prod, qtd){     
+    total5  = qtd * prod 
+    document.getElementById('subSabaoEmPo').value = total5.toFixed(2)
 }
-function detergenteCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function detergenteCalc(prod, qtd){   
+    total6 = qtd * prod
+    document.getElementById('subDetergente').value = total6.toFixed(2)
 }
-function shampooCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function shampooCalc(prod, qtd){         
+   total7 = qtd * prod
+   document.getElementById('subShampoo').value = total7.toFixed(2)
 }
-function saboneteCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function saboneteCalc(prod, qtd){            
+    total8= qtd * prod
+    document.getElementById('subSabonete').value = total8.toFixed(2)
 }
 function papelHigienicoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+           
+    total9 = qtd * prod
+    document.getElementById('subPapelHigienico').value = total9.toFixed(2)
 }
 function cremeDentalCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+            
+    total10= qtd * prod
+    document.getElementById('subCremeDental').value = total10.toFixed(2)
 }
-function acucarCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function acucarCalc(prod, qtd){       
+    total11= qtd * prod
+    document.getElementById('subPapelHigienico').value = total11.toFixed(2)
 }
-function arrozCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+function arrozCalc(prod, qtd){          
+    total12 = qtd * prod
+    document.getElementById('subArroz').value = total12.toFixed(2)
 }
 function feijaoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+          
+    total13 = qtd * prod
+    document.getElementById('subFeijao').value = total13.toFixed(2)
 }
 function oleoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+           
+    total14= qtd * prod
+    document.getElementById('subOleo').value = total14.toFixed(2)
 }
 function macarraoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+           
+    total15= qtd * prod
+    document.getElementById('subMacarrao').value = total15.toFixed(2)
 }
 function cafeCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+             
+    total16 = qtd * prod;
+    document.getElementById('subCafe').value = total16.toFixed(2)
 }
 function filtroCafeCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+          
+    total17= qtd * prod
+    document.getElementById('subFiltroCafe').value = total17.toFixed(2)
 }
 function leiteCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+         
+    total18 = qtd * prod
+    document.getElementById('subLeite').value = total18.toFixed(2)
 }
 function manteigaCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+             
+    total19 = qtd * prod
+    document.getElementById('subManteiga').value = total19.toFixed(2)
 }
 function margarinaCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+             
+    total20 = qtd * prod
+    document.getElementById('subMargarina').value = total20.toFixed(2)
 }
 function yogurteCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+              
+    total21 = qtd * prod 
+    document.getElementById('subYorgute').value = total21.toFixed(2)
 }
-// function biscoitoRecheadoCalc(prod, qtd)
-// prod = document.getElementsByName('').value;
-// qtd{} = document.getElementsByName('').value
-// total 
+function biscoitoRecheadoCalc(prod, qtd){
+              
+    total22  = qtd * prod
+    document.getElementById('subBiscoitoRecheado').value = total22.toFixed(2)
+} 
 function biscoitoSalgadoCalc(prod, qtd){
-    prod = document.getElementsByName('').value;
-    qtd = document.getElementsByName('').value
-    total 
+      
+    total23 = qtd * prod
+    document.getElementById('subBiscoitoSalgado').value = total23.toFixed(2)
 }
 function paoFormaCalc(prod, qtd){
-
+       
+    total24 = qtd * prod
+    document.getElementById('subPaoForma').value = total24.toFixed(2)
+}   
+function batataCalc(prod, qtd){
+    
+    total25= qtd * prod
+    document.getElementById('subBatata').value = total25.toFixed(2)
+}
+function alhoCalc(prod, qtd){
+  
+    total26 = qtd * prod;
+    document.getElementById('subAlho').value = total26.toFixed(2)
 }
